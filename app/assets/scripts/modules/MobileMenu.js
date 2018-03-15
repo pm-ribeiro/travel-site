d4f6db8e6b85457ee9751e800da09007a024c659
pm-ribeiro <pmribeiro@inf.ufpel.edu.br>
2018-03-15 13:42:14 -0300

    finish the mobile menu icon for small screens

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -25,6 +25,7 @@ class MobileMenu {
 		//alert(this);
 		this.menuContent.toggleClass("site-header__menu-content--is-visible");
 		this.siteHeader.toggleClass("site-header--is-expanded");
+		this.menuIcon.toggleClass("site-header__menu-icon--close-x");
 	}
 
 }
