e781e7acc0256c938fbcba883f0ef8c85714c1dc
pm-ribeiro <pmribeiro@inf.ufpel.edu.br>
2018-03-15 13:15:07 -0300

    styling mobile menu, for small screens

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,3 @@
-//var $ = require('jquery');
-//var Person = require('./modules/Person');
+import MobileMenu from './modules/MobileMenu';
 
-//ecma script 6
-import Person from './modules/Person';
-
-class Adult extends Person {
-	payTaxes(){
-		console.log(this.name + " now owes 0$ in taxes");
-	}
-}
-
-alert("abc 123");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "orange");
-jane.greet();
-jane.payTaxes();
-//$("h1").remove();
+var mobileMenu = new MobileMenu();
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+
+class MobileMenu {
+
+	constructor(){
+		//new way - no jquery spaghetti
+
+		//selecting the elements with jquery
+		this.siteHeader = $(".site-header");
+		this.menuIcon = $(".site-header__menu-icon");
+		this.menuContent = $(".site-header__menu-content");
+		this.events();
+	}
+
+	//new way - method
+	events(){
+		//listing all events that we want to watch for
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
+		
+	}
+
+	//new way - method
+	toggleTheMenu(){
+		//console.log("clicked");
+		//alert(this);
+		this.menuContent.toggleClass("site-header__menu-content--is-visible");
+		this.siteHeader.toggleClass("site-header--is-expanded");
+	}
+
+}
+
+export default MobileMenu;
