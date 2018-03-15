3d0f67cc83e1fd11f448d1044837d8e9b2242b89
pm-ribeiro <pmribeiro@inf.ufpel.edu.br>
2018-03-15 14:19:57 -0300

    start working in the reveal on scroll feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
+
+//creating objects
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
